9512f7f0d8645a54e3ebb257812c7d620e22c8b8
xiaosu <xiaosu@gmail.com>
2023-10-02 15:56:39 +0800

    第一次配置好所有的数据项

---
+++ app/src/main/java/com/example/coolweather/db/Province.java
@@ -0,0 +1,30 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName; //记录省的名字
+    private int provinceCode;//记录县的代码
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+    public String getprovinceName(){
+        return provinceName;
+    }
+    public void setProvinceName(){
+        this.provinceName = provinceName;
+    }
+}
